57a97d4d504f1fd5bf9a34409c9eb7a026b6cb7a
MJ Suarez <msmjsuarez@gmail.com>
2022-02-22 00:00:56 +0800

    Getting Started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Main />
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      
     </div>
   );
 }
